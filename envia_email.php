57fb67e4fd18411a58b851b3ccc3d740e01267fa
monsyc <silva.castro.mony@gmail.com>
2016-01-04 00:15:40 -0800

    2nd release
    need to add a paragraph after php add book and picture and fix some
    parts

---
+++ envia_email.php
@@ -0,0 +1,26 @@
+<?php
+
+	$name = $_POST['name'];
+  $email = $_POST['email'];
+  $text = $_POST['text'];
+  $to = "silva.castro.mony@gmail.com";
+  $subject = 'Message from Local 10 Site ';
+
+  $message = "From: $name\n E-Mail: $email\n Message:\n $text";
+  
+  $message = str_replace("\n.", "\n..", $message);
+  
+  $headers = 'From: silva.castro.mony@gmail.com' . "\r\n" .
+    'Reply-To: silva.castro.mony@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+  $success = mail($to, $subject, $message, $headers);
+  if(!$success) {   
+       echo "Error";   
+  } else {
+      echo "Success";
+  }
+  
+	header('Location: contactanos.html');
+
+?>
