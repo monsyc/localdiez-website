28fdab6e4a274b08cf7047ab0a714524fd02ecf4
rafanoel <rafanoel@gmail.com>
2016-11-03 23:21:18 -0700

    Updated Architecture Screen
    Added arquitecture JS file.
    Added place holder PNG file.
    Loads modal pictures upon request.

---
+++ js/arquitectura.js
@@ -0,0 +1,55 @@
+$( document ).ready(function() {
+    loadModalImages();
+});
+
+function loadModalImages() {
+
+    $("#myModal2 .image-1").css('content', 'url("images4/FamiliaCleary-2014-1.jpg")');
+    $("#myModal2 .image-2").css('content', "url('images4/FamiliaCleary-2014-2.jpg')");
+    $("#myModal2 .image-3").css('content', "url('images4/FamiliaCleary-2014-3.jpg')");
+
+
+    $("#myModal .image-1").css('content', 'url("images4/FamiliaCano-2014-1.jpg")');
+    $("#myModal .image-2").css('content', 'url("images4/FamiliaCano-2014-2.jpg")');
+    $("#myModal .image-3").css('content', 'url("images4/FamiliaCano-2014-3.jpg")');
+    $("#myModal .image-4").css('content', 'url("images4/FamiliaCano-2014-4.jpg")');
+    $("#myModal .image-5").css('content', 'url("images4/FamiliaCano-2014-5.jpg")');
+
+
+    $("#myModal3 .image-1").css('content', 'url("images4/Ingeniat-2014-1.jpg")');
+    $("#myModal3 .image-2").css('content', 'url("images4/Ingeniat-2014-2.jpg")');
+    $("#myModal3 .image-3").css('content', 'url("images4/Ingeniat-2014-3.jpg")');
+    $("#myModal3 .image-4").css('content', 'url("images4/Ingeniat-2014-4.jpg")');
+
+
+    $("#myModal4 .image-1").css('content', 'url("images4/FamiliaGarza-2014-1.jpg")');
+    $("#myModal4 .image-2").css('content', 'url("images4/FamiliaGarza-2014-2.jpg")');
+    $("#myModal4 .image-3").css('content', 'url("images4/FamiliaGarza-2014-3.jpg")');
+    $("#myModal4 .image-4").css('content', 'url("images4/FamiliaGarza-2014-4.jpg")');
+    $("#myModal4 .image-5").css('content', 'url("images4/FamiliaGarza-2014-5.jpg")');
+
+
+    $("#myModal5 .image-1").css('content', 'url("images4/Olinala-2011-1.jpg")');
+    $("#myModal5 .image-2").css('content', 'url("images4/Olinala-2011-2.jpg")');
+    $("#myModal5 .image-3").css('content', 'url("images4/Olinala-2011-3.jpg")');
+    $("#myModal5 .image-4").css('content', 'url("images4/Olinala-2011-4.jpg")');
+    $("#myModal5 .image-5").css('content', 'url("images4/Olinala-2011-5.jpg")');
+    $("#myModal5 .image-6").css('content', 'url("images4/Olinala-2011-6.jpg")');
+    $("#myModal5 .image-7").css('content', 'url("images4/Olinala-2011-7.jpg")');
+
+
+    $("#myModal6 .image-1").css('content', 'url("images4/Lanx-2010-1.jpg")');
+    $("#myModal6 .image-2").css('content', 'url("images4/Lanx-2010-2.jpg")');
+    $("#myModal6 .image-3").css('content', 'url("images4/Lanx-2010-3.jpg")');
+
+
+    $("#myModal7 .image-1").css('content', 'url("images4/SanJuan-2008-1.jpg")');
+    $("#myModal7 .image-2").css('content', 'url("images4/SanJuan-2008-2.jpg")');
+    $("#myModal7 .image-3").css('content', 'url("images4/SanJuan-2008-3.jpg")');
+
+
+    $("#myModal8 .image-1").css('content', 'url("images4/CentroComercial-2006-1.jpg")');
+    $("#myModal8 .image-2").css('content', 'url("images4/CentroComercial-2006-2.jpg")');
+    $("#myModal8 .image-3").css('content', 'url("images4/CentroComercial-2006-3.jpg")');
+}
+
