e4b0d4fbfad139a67bd5641e27f9bfaefb908822
rafanoel <rafanoel@gmail.com>
2016-11-05 15:15:44 -0700

    Added nav to arquitecture JS file
    Added nav to arquitecture JS file

--- js/arquitectura.js
+++ js/arquitectura.js
@@ -1,3 +1,23 @@
+/* MENU - Open the drawer when the menu icon is clicked. */
+
+var menu = document.querySelector('#menu');
+var main = document.querySelector('main');
+var drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function(e) {
+  drawer.classList.toggle('open');
+  e.stopPropagation();
+});
+
+main.addEventListener('click', function() {
+  drawer.classList.remove('open');
+});
+
+
+
+
+/* Load Images to Modal */
+
 $( document ).ready(function() {
     loadModalImages();
 });
