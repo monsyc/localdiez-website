4b3d7418e1a13df5512ff05436f64a076af02285
Rafael Leon Barrera <rafanoel@gmail.com>
2016-11-08 18:28:48 -0800

    Changed all images in index html file.
    Changed images to load them in the background.

--- dev/js/index.js
+++ dev/js/index.js
@@ -15,9 +15,19 @@ function loadImage(clase, imagen) {
 
 function loadModalImages() {
 
+    loadImage("#carrousel .image-1","img/Home_Carrousel-1.jpg");
+    loadImage("#carrousel .image-2","img/Home_Carrousel-2.jpg");
+    loadImage("#carrousel .image-3","img/Home_Carrousel-3.jpg");
+    loadImage("#carrousel .image-4","img/Home_Carrousel-4.jpg");
+
     loadImage("#carrousel2 .image-1","img/Home_Carrousel-1-r.jpg");
     loadImage("#carrousel2 .image-2","img/Home_Carrousel-2-r.jpg");
     loadImage("#carrousel2 .image-3","img/Home_Carrousel-3-r.jpg");
     loadImage("#carrousel2 .image-4","img/Home_Carrousel-4-r.jpg");
+
+    loadImage("#projects .image-1","img/Home_Porcelanite-Cihac-2016.jpg");
+    loadImage("#projects .image-2","img/Home_Lamosa-Cihac-2016.jpg");
+    loadImage("#projects .image-3","img/Home_Firenze-Cihac-2016.jpg");
+    loadImage("#projects .image-4","img/Home_Gante-Cihac-2016.jpg");
 
 }
