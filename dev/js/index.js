bc5b21451815d343150b443977e0d2e8befd87ae
Rafael Leon Barrera <rafanoel@gmail.com>
2016-11-10 18:35:43 -0800

    Returned index HTML file to the image carousel.
    An image carousel is now displayed, instead of the video.

--- dev/js/index.js
+++ dev/js/index.js
@@ -38,55 +38,55 @@ function loadModalImages() {
 
 
 // This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+// var tag = document.createElement('script');
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// tag.src = "https://www.youtube.com/iframe_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '100%',
-        width: '100%',
-        videoId: 'ZELOoPGt6c4',
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange,
-            'onPlaybackQualityChange': onPlayerPlaybackQualityChange,
-            'onError': onPlayerError
-        }
-    });
-}
+// var player;
+// function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//         height: '100%',
+//         width: '100%',
+//         videoId: 'ZELOoPGt6c4',
+//         events: {
+//             'onReady': onPlayerReady,
+//             'onStateChange': onPlayerStateChange,
+//             'onPlaybackQualityChange': onPlayerPlaybackQualityChange,
+//             'onError': onPlayerError
+//         }
+//     });
+// }
 
 // he API will call this function when the video player is ready.
-function onPlayerReady(event) {
-    event.target.setVolume(1);
-    event.target.playVideo();
-    event.target.setPlaybackQuality('hd720');
-}
+// function onPlayerReady(event) {
+//     event.target.setVolume(1);
+//     event.target.playVideo();
+//     event.target.setPlaybackQuality('hd720');
+// }
 
 // 5. The API calls this function when the player's state changes.
 //    The function indicates that when playing a video (state=1),
 //    the player should play for six seconds and then stop.
-function onPlayerStateChange(event) {
-    // if (event.data == YT.PlayerState.PLAYING && !done) 
-    console.log("Player State Changed: " + event.data);
-    if (event.data == YT.PlayerState.BUFFERING) {
-        event.target.setPlaybackQuality('hd720');
-    }
-}
+// function onPlayerStateChange(event) {
+//     // if (event.data == YT.PlayerState.PLAYING && !done) 
+//     console.log("Player State Changed: " + event.data);
+//     if (event.data == YT.PlayerState.BUFFERING) {
+//         event.target.setPlaybackQuality('hd720');
+//     }
+// }
 
-function stopVideo() {
-    player.stopVideo();
-}
+// function stopVideo() {
+//     player.stopVideo();
+// }
 
-function onPlayerPlaybackQualityChange(event) {
-    console.log("onPlayerPlaybackQualityChange: " + event.data);
-}
+// function onPlayerPlaybackQualityChange(event) {
+//     console.log("onPlayerPlaybackQualityChange: " + event.data);
+// }
 
-function onPlayerError(event) {
-    console.log("onPlayerError: " + event.data);
-}
+// function onPlayerError(event) {
+//     console.log("onPlayerError: " + event.data);
+// }
